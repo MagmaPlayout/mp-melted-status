5d67c9090ae7be20babe1ba718a3e1abe5157236
rombusevil <rombusevil@github.com>
2017-06-22 17:09:48 -0300

    Added nbproject with first untested and incomplete draft

---
+++ src/meltedstatus/MeltedStatus.java
@@ -0,0 +1,108 @@
+package meltedstatus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import playoutCore.ConfigurationManager;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.exceptions.JedisConnectionException;
+
+
+/**
+ * Connects to a running melted instance and queries the STATUS.
+ * STATUS is a unique command, so I'm not going to use mpc-meltedBackend for this.
+ * This way is clearer and simpler.
+ * 
+ * @author rombus
+ */
+public class MeltedStatus {
+    private Socket socket;
+    private PrintWriter writer;
+    private BufferedReader reader;
+    private boolean running;
+    
+    
+    public static void main(String[] args) {
+        MeltedStatus ms = new MeltedStatus();
+        ms.run();
+    }
+    
+    private void run(){
+        // connect to telnet melted host
+        // send STATUS cmd
+        // parse output line by line
+        // General config
+        
+        Logger logger = Logger.getLogger(MeltedStatus.class.getName());
+        ConfigurationManager cfg = ConfigurationManager.getInstance();
+        cfg.init(logger);
+        
+        /**
+         * Connect to Redis publisher server.
+         */
+        Jedis redisPublisher = new Jedis(cfg.getRedisHost(), cfg.getRedisPort(), cfg.getRedisReconnectionTimeout());
+        try {
+            while(connectToRedisPublisher(logger, cfg, redisPublisher)){
+                logger.log(Level.SEVERE, "MeltedStatus - ERROR: Could not connect to the Redis server. Is it running?\nExiting...");
+                Thread.sleep(5000);
+            }
+        } catch (InterruptedException ex) {
+            System.exit(1);
+        }
+        String mstaChannel = "MSTA"; // TODO agregar una nueva config MSTA (melted status) como canal de redis
+        
+        
+        try {
+            socket = new Socket(cfg.getMeltedHost(), cfg.getMeltedPort());
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
+            writer.println("STATUS");        // Send telnet command
+            running=true;
+            while(running){
+                String line = reader.readLine(); // Blocking method
+                if(line != null){
+                    logger.log(Level.INFO, "MeltedStatus - : Could not connect to the Melted server. Retries exhausted. \nExiting...");
+                    // Do something with line
+                    // check if melted is running, what clip is playing, how many clips remains ...
+                    // redisPublisher.publish(mstaChannel, "blabal");
+                }
+            }
+        } catch (IOException ex) {
+            //TODO
+        }
+    }
+    
+    public void reconnect(){
+        //TODO: reconn routine
+    }
+    
+    public void disconnect(){
+        try {
+            running = false;
+            writer.close();
+            reader.close();
+            socket.close();
+        } catch (IOException ex) {
+            //TODO
+            ex.printStackTrace();
+        }
+    }
+    
+    public static boolean connectToRedisPublisher(Logger logger, ConfigurationManager cfg, Jedis redisPublisher){
+        logger.log(Level.INFO, "MeltedStatus - Attempt to connect to Redis Pub/Sub server...");
+        try {
+            redisPublisher.connect();
+        }
+        catch(JedisConnectionException e){
+            logger.log(Level.SEVERE, "MeltedStatus - ERROR: Could not connect to the Redis server. Is it running?\nExiting...");
+            return false;
+        }
+        
+        return true;
+    }
+}
